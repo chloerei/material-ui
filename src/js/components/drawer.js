c0e950914c66637e972e4afad3b5bc54ace86c2f
Rei <chloerei@gmail.com>
2020-03-25 17:13:45 +0800

    edit drawer js for install logic

--- src/js/components/drawer.js
+++ src/js/components/drawer.js
@@ -1,9 +1,41 @@
 import { delegateEvent } from './utils.js'
 
-delegateEvent(document, '[data-toggle="drawer"]', 'click', function(event){
-  document.querySelector(this.dataset.target).classList.toggle('open')
-})
+class Drawer {
+  constructor(element) {
+    this.element = element
+  }
 
-delegateEvent(document, '.drawer-background', 'click', function(event){
-  this.closest('.drawer').classList.remove('open')
+  static install(element) {
+    if (!element.drawer) {
+      element.drawer = new Drawer(element)
+    }
+    return element.drawer
+  }
+
+  toggle() {
+    if (this.element.classList.contains('open')) {
+      this.close()
+    } else {
+      this.open()
+    }
+  }
+
+  open() {
+    this.element.classList.add('open')
+    this.background = document.createElement('div')
+    this.background.classList.add('drawer-background')
+    this.background.addEventListener('click', () => {
+      this.close()
+    })
+    this.element.appendChild(this.background)
+  }
+
+  close() {
+    this.element.classList.remove('open')
+    this.background.remove()
+  }
+}
+
+delegateEvent(document, '[data-toggle="drawer"]', 'click', function(event){
+  Drawer.install(document.querySelector(this.dataset.target)).toggle()
 })
