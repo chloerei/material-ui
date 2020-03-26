03bd2d749fa91af1491e27577351e8c0720b1700
Rei <chloerei@gmail.com>
2020-03-26 17:10:34 +0800

    Add drawer example

--- themes/campo-ui/scripts/example.js
+++ themes/campo-ui/scripts/example.js
@@ -2,7 +2,7 @@ var util = require('hexo-util')
 
 hexo.extend.tag.register('example', function(args, content){
   return `
-    <div class="example">
+    <div class="example ${args.join(' ')}">
       ${content}
     </div>
     ${util.highlight(content, { gutter: false, wrap: false, lang: 'html', hljs: true })}
