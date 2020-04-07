1f810001b400d684aa3519e37bfdceebd44db6ab
Rei <chloerei@gmail.com>
2020-04-07 15:57:01 +0800

    let drawer persistence able

--- src/js/controllers/drawer_controller.js
+++ src/js/controllers/drawer_controller.js
@@ -6,6 +6,28 @@ export default class extends Controller {
     background.classList.add('drawer-background')
     background.addEventListener('click', () => this.close())
     this.element.insertAdjacentElement('afterend', background)
+
+    if (this.persistenceEnabled()) {
+      this.restore()
+    }
+  }
+
+  persistenceEnabled() {
+    return this.data.get('persistence') == 'true' && window.innerWidth > 480
+  }
+
+  store() {
+    if (this.persistenceEnabled()) {
+      localStorage.setItem('drawerOpened', this.element.classList.contains('drawer--open'));
+    }
+  }
+
+  restore() {
+    if (this.persistenceEnabled()) {
+      if (localStorage.getItem('drawerOpened') == 'true') {
+        this.open()
+      }
+    }
   }
 
   toggle() {
@@ -14,13 +36,16 @@ export default class extends Controller {
     } else {
       this.open()
     }
+    this.store()
   }
 
   open() {
     this.element.classList.add('drawer--open')
+    this.store()
   }
 
   close() {
     this.element.classList.remove('drawer--open')
+    this.store()
   }
 }
