6ff229428d79f0e912029589ca5f336771a9e101
Rei <chloerei@gmail.com>
2020-06-07 19:30:40 +0800

    Add chip component

--- src/js/campo-ui.js
+++ src/js/campo-ui.js
@@ -8,6 +8,7 @@ import FloatingActionController from './controllers/floating_action_controller'
 import SnackbarController from './controllers/snackbar_controller'
 import ToggleController from './controllers/toggle_controller'
 import SheetController from './controllers/sheet_controller'
+import ChipController from './controllers/chip_controller'
 
 const application = Application.start()
 application.register("dialog", DialogController)
@@ -17,5 +18,6 @@ application.register("floating-action", FloatingActionController)
 application.register("snackbar", SnackbarController)
 application.register("toggle", ToggleController)
 application.register("sheet", SheetController)
+application.register("chip", ChipController)
 
 export { application }
