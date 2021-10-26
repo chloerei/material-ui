6c20ef8c7829a53a53d4f8d0b7e6029d12326d3c
Rei <chloerei@gmail.com>
2021-10-26 21:37:01 +0800

    refactory sheet

--- src/js/controllers/sheet_controller.js
+++ src/js/controllers/sheet_controller.js
@@ -17,12 +17,12 @@ export default class extends Controller {
   open() {
     this.element.classList.add('sheet--open')
     animate(this.element.querySelector('.sheet__scrim'), 'animate--fade-in')
-    animate(this.element.querySelector('.sheet__container'), 'animate--sheet-in')
+    animate(this.element, 'animate--sheet-in')
   }
 
   close() {
     animate(this.element.querySelector('.sheet__scrim'), 'animate--fade-out')
-    animate(this.element.querySelector('.sheet__container'), 'animate--sheet-out', () => {
+    animate(this.element, 'animate--sheet-out', () => {
       this.element.classList.remove('sheet--open')
     })
   }
